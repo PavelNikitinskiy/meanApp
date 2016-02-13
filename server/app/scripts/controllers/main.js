00badf6794e80edcff9ef2106cb0ea5adc4cbb1c
Pavel N <nikitinskypavel@gmail.com>
2016-02-13 16:39:56 +0200

    fix angular install

---
+++ server/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serverApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the serverApp
+ */
+angular.module('serverApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
