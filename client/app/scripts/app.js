85581c4b0f0d2f48a1fdb65321d047cf7c0c6a70
Pavel N <nikitinskypavel@gmail.com>
2016-02-14 15:50:51 +0200

    need debug finish

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,35 +10,67 @@
  */
 angular
   .module('clientApp', [
-    'ngRoute', 'restangular'
+    'ngRoute',
+    'restangular'
   ])
-  .factory('MovieRestangular', function(Restangular) {
-    return Restangular.withConfig(function(RestangularConfigurer) {
-      RestangularConfigurer.setRestangularFields({  id: '_id' });
-    });
-  })
-  .factory('Movie', function(MovieRestangular){
-    return MovieRestangular.service('movie');
-  })
   .config(function ($routeProvider, RestangularProvider) {
+
     RestangularProvider.setBaseUrl('http://localhost:3000');
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        controller: 'MainCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controller: 'AboutCtrl'
       })
       .when('/movies', {
         templateUrl: 'views/movies.html',
-        controller: 'MoviesCtrl',
-        controllerAs: 'movies'
+        controller: 'MoviesCtrl'
+      })
+      .when('/create/movie', {
+        templateUrl: 'views/movie-add.html',
+        controller: 'MovieAddCtrl'
+      })
+      .when('/movie/:id', {
+        templateUrl: 'views/movie-view.html',
+        controller: 'MovieViewCtrl'
+      })
+      .when('/movie/:id/edit', {
+        templateUrl: 'views/movie-edit.html',
+        controller: 'MovieEditCtrl'
+      })
+      .when('/movie/:id/delete', {
+        templateUrl: 'views/movie-delete.html',
+        controller: 'MovieDeleteCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .factory('MovieRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+  .factory('Movie', function(MovieRestangular) {
+    return MovieRestangular.service('movie');
+  })
+  .directive('youtube', function(){
+    return {
+      restrict: 'E',
+      scope: {
+        src: '='
+      },
+      templateUrl: "views/youtube.html"
+    };
+  })
+  .filter('trusted', function($sce){
+    return function(url){
+      return $sce.trustAsResourceUrl(url);
+    };
   });
--- client/app/scripts/controllers/movies.js
+++ client/app/scripts/controllers/movies.js
@@ -8,6 +8,6 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MoviesCtrl', function ($scope, Movie) {
-    $scope.movies = Movie.getList().$object; // read restangular api
+  .controller('MoviesCtrl', function ( $scope, Movie ) {
+     $scope.movies = Movie.getList().$object;
   });
