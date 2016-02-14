814024a2d80319d842be4c8633778122d89693c8
Pavel N <nikitinskypavel@gmail.com>
2016-02-14 15:54:03 +0200

    Remove duplicated directory

--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  movie: require('./Movie.js')
-
-};
--- server/seed.js
+++
@@ -1,7 +0,0 @@
-[
-  {"title":"The Lord of the Rings - Best Scene",
-  "url":"https://www.youtube.com/watch?v=rCZ3SN65kIs"},
-  {"title":"The Lord of the Rings: The Return of the King Official Trailer","url":"https://www.youtube.com/watch?v=r5X-hFf6Bwo"},
-  {"title":"The Lord of the Rings: The Two Towers (2002) Official Trailer","url":"https://www.youtube.com/watch?v=LbfMDwc4azU"},
-  {"title":"The Lord of the Rings: The Fellowship of the Ring Official Trailer ","url":"https://www.youtube.com/watch?v=V75dMMIW2B4"}
-]
